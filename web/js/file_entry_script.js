7ab32de09bd52d31659788e6c822273e71f4ed24
Dima Smirnov <diamond101001@gmail.com>
2023-06-02 20:13:49 +0300

    update js files. code refactoring

--- web/js/file_entry_script.js
+++ web/js/file_entry_script.js
@@ -1,28 +1,36 @@
 document.getElementById('research_date').valueAsDate = new Date();
 
+
+// функция сбора данных из формы, проверки их правильности и
 async function serializeForm(formNode) {
+    // получение данных из формы
     let formData = new FormData(formNode);
     let values = Object.fromEntries(formData.entries());
 
+    // блок формы
     const formToCHeck = document.querySelector('form')
 
+    // блоки модального окна
     let modal_title = document.getElementById('modal_title')
     let modal_text = document.getElementById('modal_body_text')
 
+    // проверка валидности данных
     if (formToCHeck.checkValidity()){
         modal_title.innerHTML = 'Успешно!'
         modal_text.innerHTML = 'Файл с данными успешно сохранен!'
 
         document.getElementById('research_date').valueAsDate = new Date();
+        // заполнение шаблона полученными данными
         await eel.fill_document(values);
     }else {
         modal_title.innerHTML = 'Ошибка!'
         modal_text.innerHTML = 'Одно или несколько обязательных полей формы остались не заполнены!'
     }
-
+    // открытие модального окна
     location.href = '#openModal'
 }
 
+// функция отключения блока cytological_examination, при virgin === True
 function is_virgin_changed(){
     let cytological_examination = document.getElementById('cytological-examination')
 
@@ -36,17 +44,18 @@ function is_virgin_changed(){
     }
 }
 
-
+// функция подтверждения формы
 function handleFormSubmit(event) {
     event.preventDefault()
     serializeForm(applicantForm)
 }
 
-
+// назначение функции подтверждения форме
 const applicantForm = document.getElementById('form')
 applicantForm.addEventListener('submit', handleFormSubmit)
 
 
+// назначение команды закрытия модального окна
 const btn_close = document.getElementById('close_modal')
 btn_close.onclick = () => {
     location.href = '#close'
--- web/js/registration_script.js
+++ web/js/registration_script.js
@@ -1,4 +1,5 @@
 const applicantForm = document.getElementById('registration_form')
+// функция сохранения пути к директории хранения отчетов.
 applicantForm.onsubmit = async function(evt){
     const path_to_save = document.getElementById('path_to_save').value
     const formToCHeck = document.querySelector('form')
@@ -10,7 +11,7 @@ applicantForm.onsubmit = async function(evt){
     await eel.registrate_path_to_save(path_to_save);
 }
 
-
+// закрытие модального окна
 const btn_close = document.getElementById('close_modal')
 btn_close.onclick = () => {
     location.href = '#close'
